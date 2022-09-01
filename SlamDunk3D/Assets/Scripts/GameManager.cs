21f1ce102edde52b99d7e2b24e5f4a104d7b96a6
bahauslusoy <105641542+bahauslusoy@users.noreply.github.com>
2022-09-01 20:39:15 +0300

    Add GameManager Script

---
+++ SlamDunk3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject platform;
+    [SerializeField] private Image[] missions ;
+    [SerializeField] private Sprite missionComp ;
+    [SerializeField] private int requiredThrowBall ;
+
+    void Start()
+    {
+        for( int i = 0; i< requiredThrowBall; i++ )
+        {
+            missions[i].gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            platform.transform.position = Vector3.Lerp(platform.transform.position, new Vector3(platform.transform.position.x - 0.3f,
+            platform.transform.position.y, platform.transform.position.z), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            platform.transform.position = Vector3.Lerp(platform.transform.position, new Vector3(platform.transform.position.x + 0.3f,
+            platform.transform.position.y, platform.transform.position.z), 0.1f);
+        }
+    }
+}
