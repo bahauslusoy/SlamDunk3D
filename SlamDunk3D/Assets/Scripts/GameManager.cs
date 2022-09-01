79e10d376bb8f09a679c2ff121fa8587999a599a
bahauslusoy <105641542+bahauslusoy@users.noreply.github.com>
2022-09-01 21:01:19 +0300

    Update GameManageScript

--- SlamDunk3D/Assets/Scripts/GameManager.cs
+++ SlamDunk3D/Assets/Scripts/GameManager.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject platform;
-    [SerializeField] private Image[] missions ;
-    [SerializeField] private Sprite missionComp ;
-    [SerializeField] private int requiredThrowBall ;
+    [SerializeField] private Image[] missions;
+    [SerializeField] private Sprite missionComp;
+    [SerializeField] private int requiredThrowBall;
+
+    private int basketNumber;
 
     void Start()
     {
-        for( int i = 0; i< requiredThrowBall; i++ )
+        for (int i = 0; i < requiredThrowBall; i++)
         {
             missions[i].gameObject.SetActive(true);
         }
@@ -31,5 +33,20 @@ public class GameManager : MonoBehaviour
             platform.transform.position = Vector3.Lerp(platform.transform.position, new Vector3(platform.transform.position.x + 0.3f,
             platform.transform.position.y, platform.transform.position.z), 0.1f);
         }
+    }
+
+    public void Basket()
+    {
+        basketNumber++;
+        missions[basketNumber - 1].sprite = missionComp;
+        if (basketNumber == requiredThrowBall)
+        {
+
+        }
+    }
+
+    public void Lose()
+    {
+        Debug.Log("lOse");
     }
 }
