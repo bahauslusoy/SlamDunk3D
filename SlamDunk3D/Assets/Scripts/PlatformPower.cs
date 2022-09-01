7ba7ae51eec2e7b457a34731fb7b4bc23dc5d54a
bahauslusoy <105641542+bahauslusoy@users.noreply.github.com>
2022-09-01 16:11:02 +0300

    Add
    PlatformPower Script

--- SlamDunk3D/Assets/Scripts/PlatformPower.cs
+++ SlamDunk3D/Assets/Scripts/PlatformPower.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlatformPower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float angle;
+    [SerializeField] private float power;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(angle,90,0)* power, ForceMode.Force);
     }
 }
